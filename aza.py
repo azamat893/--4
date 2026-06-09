b22dc182ec1ff0c89764ddb09043b0f59264162f
Student <509-6@intc.local>
2026-06-09 15:52:45 +0500

    new_branch

--- aza.py
+++ aza.py
@@ -1,2 +1,34 @@
-ljkgfkjgf
-jdf';sjf'
+from tkinter import *
+from tkinter import messagebox
+
+def calculate():
+    try:
+        number = float(entry.get())
+
+        square = number ** 2
+        cube = number ** 3
+
+        result_label.config(
+            text=f"Квадраты: {square}\nКубы: {cube}"
+        )
+
+    except ValueError:
+        messagebox.showerror("Қате", "Сан енгізіңіз!")
+
+window = Tk()
+window.title("Санның квадратын және кубын есептеу")
+window.geometry("300x200")
+
+label = Label(window, text="Санды енгізіңіз:")
+label.pack(pady=5)
+
+entry = Entry(window, width=20)
+entry.pack(pady=5)
+
+button = Button(window, text="Есептеу", command=calculate)
+button.pack(pady=10)
+
+result_label = Label(window, text="Нәтиже:")
+result_label.pack(pady=10)
+
+window.mainloop()
